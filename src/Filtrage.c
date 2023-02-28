23dbdc46d675118b25bfb33661fb3cb0716f522b
JohanST7 <johan.comte7@gmail.com>
2023-02-28 15:54:28 +0100

    a modifier

---
+++ src/Filtrage.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+
+float mask;
+
+printf("Entrez le masque pour filtrez les adresses IP")
+scanf("%f", &mask);
+
+void filter_ip(char* filename, float* mask) {
+    FILE *fp;
+    char line[100];
+    fp = fopen(filename, "r");
+    while (fgets(line, sizeof(line), fp)) {
+        char *ip_str = strtok(line, " \n");
+        if (ip_str == NULL) {
+            continue;
+        }
+        struct in_addr ip_addr;
+        inet_pton(AF_INET, ip_str, &ip_addr);
+        char *netmask_str = strtok(NULL, " \n");
+        if (netmask_str == NULL) {
+            continue;
+        }
+        struct in_addr netmask_addr;
+        inet_pton(AF_INET, netmask_str, &netmask_addr);
+        struct in_addr masked_ip;
+        masked_ip.s_addr = ip_addr.s_addr & netmask_addr.s_addr;
+        char *masked_ip_str = inet_ntoa(masked_ip);
+        if (strcmp(masked_ip_str, mask) == 0) {
+            printf("%s\n", ip_str);
+        }
+    }
+    fclose(fp);
+}
